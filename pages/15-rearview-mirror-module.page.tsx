f87562e65baa5d3d45781922fa2e3adbbbcd2666
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2026-08-25 00:05:58 +0530

    feat: Add rearview mirror module system block diagram (#4)

---
+++ pages/15-rearview-mirror-module.page.tsx
@@ -0,0 +1,6 @@
+import { SysBlockDiagram } from "lib/SysBlockDiagram"
+import { rearviewMirrorModule } from "lib/generated/catalog"
+
+export default function RearviewMirrorModulePage() {
+  return <SysBlockDiagram definition={rearviewMirrorModule} />
+}
